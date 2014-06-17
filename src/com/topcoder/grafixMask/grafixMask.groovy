c8963230e9fb74d06212820536df651e617baee0
Ivelin Bratanov <ivelin.bratanov@ericsson.com>
2014-06-16 19:23:47 -0500

    Initialize Rectangle class to set up problem.

--- src/com/topcoder/grafixMask/grafixMask.groovy
+++ src/com/topcoder/grafixMask/grafixMask.groovy
@@ -4,5 +4,24 @@ import java.util.*
 class grafixMask {
 	void main() {
 		def grid = new boolean[50][50]
+		def Rectangles = initializeRectangles()
+	}
+	
+	Rectangle[] initializeRectangles() {
+		def Rectangles = new Rectangle[4]
+		Rectangles[0] = new Rectangle(0,2,0,2)
+		Rectangles[1] = new Rectangle(0,1,5,7)
+		Rectangles[2] = new Rectangle(10,16,16,19)
+		Rectangles[3] = new Rectangle(30,33,30,43)
+	}
+}
+
+class Rectangle {
+	def left, right, top, bottom
+	public Rectangle(l, r, t, b) {
+		this.left = l
+		this.right = r
+		this.top = t
+		this.bottom = b
 	}
 }
