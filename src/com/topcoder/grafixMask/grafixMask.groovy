41f1ebdc5d1e9bc96b6d451d701ad82a901e9d6f
Ivelin Bratanov <ivelin.bratanov@ericsson.com>
2014-06-25 18:36:53 -0400

    Finalize setup for the grafixMask problem.

--- src/com/topcoder/grafixMask/grafixMask.groovy
+++ src/com/topcoder/grafixMask/grafixMask.groovy
@@ -2,23 +2,49 @@ package com.topcoder.grafixMask
 import java.util.*
 
 class grafixMask {
-	void main() {
-		def grid = new boolean[50][50]
-		def Rectangles = initializeRectangles()
+	public static void main(String[] args) {
+		def grid = initializeEmptyGrid(50, 50)
+		def rectangles = initializeRectangles()
+		
+		grid = initializeGridWithRectangles(grid, rectangles)
 	}
 	
-	Rectangle[] initializeRectangles() {
-		def Rectangles = new Rectangle[4]
-		Rectangles[0] = new Rectangle(0,2,0,2)
-		Rectangles[1] = new Rectangle(0,1,5,7)
-		Rectangles[2] = new Rectangle(10,16,16,19)
-		Rectangles[3] = new Rectangle(30,33,30,43)
+	static boolean[][] initializeEmptyGrid(int ht, int wid) {
+		def grid = new boolean[ht][wid]
+		for (int i = 0; i < ht; i++) {
+			for (int j = 0; j < wid; j++) {
+				grid[i][j] = false
+			}
+		}
+		return grid
 	}
+	
+	static Rectangle[] initializeRectangles() {
+		def rectangles = new Rectangle[4]
+		rectangles[0] = new Rectangle(0,2,0,2)
+		rectangles[1] = new Rectangle(0,1,5,7)
+		rectangles[2] = new Rectangle(10,16,16,19)
+		rectangles[3] = new Rectangle(30,33,30,43)
+		
+		return rectangles
+	}
+	
+	static boolean[][] initializeGridWithRectangles(boolean[][] grid, Rectangle[] rectangles) {
+		for (rectangle in rectangles) {
+			for (int i = rectangle.top; i <= rectangle.bottom; i++) {
+				for (int j = rectangle.left; j <= rectangle.right; j++) {
+					grid[i][j] = true
+				}
+			}
+		}
+		return grid
+	}
+	
 }
 
 class Rectangle {
 	def left, right, top, bottom
-	public Rectangle(l, r, t, b) {
+	public Rectangle(int l, int r, int t, int b) {
 		this.left = l
 		this.right = r
 		this.top = t
